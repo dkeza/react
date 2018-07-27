6bad27bf6dd5af84449e320cf95ffa9d4a7ca242
keza <dk@citadel.rs>
2018-07-27 22:02:22 +0200

    Change template

--- indecision-app/public/scripts/app.js
+++ indecision-app/public/scripts/app.js
@@ -5,9 +5,32 @@ console.log('Test');
 // JSX - Javascript XML
 
 var template = React.createElement(
-  'p',
-  null,
-  'This is JSX from app.js!'
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item 1'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item 2'
+        )
+    )
 );
 
 var appRoot = document.getElementById('app');
--- indecision-app/src/app.js
+++ indecision-app/src/app.js
@@ -2,7 +2,17 @@ console.log('Test');
 
 // JSX - Javascript XML
 
-let template = <p>This is JSX from app.js???</p>
+let template = (
+    <div>
+        <h1>Indecision App</h1>
+        <p>This is some info</p>
+        <ol>
+            <li>Item 1</li>
+            <li>Item 2</li>
+        </ol>
+    </div>
+);
+
 
 let appRoot = document.getElementById('app');
 
