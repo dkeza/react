12bca71ae45b5369ed2b35f29992fb2fb062e39f
keza <dk@citadel.rs>
2018-08-25 14:11:20 +0200

    Extend class

--- indecision-app/src/playground/es6-classes-1.js
+++ indecision-app/src/playground/es6-classes-1.js
@@ -11,6 +11,46 @@ class Person {
     }
 }
 
-const me = new Person('Pera Zdera', 45);
-console.log(me.getDescription());
+class Student extends Person {
+    constructor(name, age, major) {
+        super(name, age);
+        this.major = major;
+    }
+    hasMajor() {
+        return !!this.major;
+    }
+    getDescription() {
+        let description = '';
+        description = super.getDescription();
 
+        if (this.hasMajor()) {
+            description += ` Their major is ${this.major}.`;
+        }
+
+        return description;
+    }
+}
+
+class Traveler extends Person {
+    constructor(name, age, location) {
+        super(name, age);
+        this.location = location;
+    }
+    hasLocation() {
+        return !!this.location;
+    }
+    getGreeting() {
+        let greeting = '';
+        greeting = super.getGreeting();
+        if (this.hasLocation()) {
+            greeting += ` I'm visiting ${this.location}.`;
+        }
+        return greeting;
+    }
+}
+
+const me = new Traveler('Pera Zdera', 45, 'Kairo');
+console.log(me.getGreeting());
+
+const me2 = new Traveler();
+console.log(me2.getGreeting());
